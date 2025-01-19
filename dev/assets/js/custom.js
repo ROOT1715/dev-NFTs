5b0c4c8919bb84391844049bccb8b4594d980174
ROOT1715 <inzhek@gmail.com>
2025-01-19 13:42:40 +0100

    commit massage

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -7,3 +7,16 @@ const toggleMobileMenu =()=>{
   btnBurger.classList.toggle('active')
 }
 btnBurger.addEventListener('click', toggleMobileMenu)
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 20,
+   // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
